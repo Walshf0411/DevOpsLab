de2a9f33962305f4eac8613aab6e6f189b321dc9
Walsh Tony Fernandes <32847434+Walshf0411@users.noreply.github.com>
2020-01-19 12:36:51 +0530

    Create addition-of-two-numbers.py

---
+++ addition-of-two-numbers.py
@@ -0,0 +1,9 @@
+def addition(a, b):
+	return a + b
+	
+if __name__ == 'main':
+	a = int(input("Enter First Number: "))
+	b = int(input("Enter second nubmer: "))
+	
+	print("Addition of numbers is: " + str(addition(a, b)))
+	
