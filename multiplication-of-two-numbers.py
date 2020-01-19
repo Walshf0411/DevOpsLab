a827d0a0c44c1d8349f096fa0d9feb4c752a174c
Walsh Fernandes <walshfernades.320@gmail.com>
2020-01-19 14:13:54 +0530

    Added multiplication program.

---
+++ multiplication-of-two-numbers.py
@@ -0,0 +1,8 @@
+def multiplication(a, b):
+    return a * b
+
+if __name__ == 'main':
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    print("Multiplication of the two numbers is: " + str(multiplication(a, b)))
